84debedd7b5841dab831f0bcc563ac45270c2f4a
Sean Stappas <seanstappas@gmail.com>
2017-11-15 23:47:58 -0500

    Added most of theory.

--- q1.py
+++ q1.py
@@ -1,5 +1,12 @@
+
+
 def q1():
     print('\n=== Question 1 ===')
+    q1a()
+
+
+def q1a():
+    pass
 
 
 if __name__ == '__main__':
--- q2.py
+++ q2.py
@@ -1,5 +1,27 @@
+import math
+
+L_a = 5e-3
+L_c = 0.3
+A = 1e-4
+N = 1000
+I = 8
+mu_0 = 4e-7 * math.pi
+
+
 def q2():
     print('\n=== Question 2 ===')
+    q2b()
+
+
+def q2b():
+    print('Flux equation: ')
+    coeff_1 = L_a / (A * mu_0)
+    coeff_2 = L_c
+    coeff_3 = N * I
+    eq = 'f(\psi) = \SI{{{:1.3e}}}{{}} \psi + {}H - {} = 0'.format(coeff_1, coeff_2, coeff_3)
+    print(eq)
+    with open('report/latex/flux_equation.txt', 'w') as f:
+        f.write(eq)
 
 
 if __name__ == '__main__':
